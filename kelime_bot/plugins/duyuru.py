57dbc444150682fbdc257b8e3067e5f3af956921
RaviBey <112951109+RaviBey@users.noreply.github.com>
2023-07-16 02:20:08 +0400

    Update duyuru.py

--- kelime_bot/plugins/duyuru.py
+++ kelime_bot/plugins/duyuru.py
@@ -5,8 +5,8 @@ from time import sleep
 from kelime_bot.plugins.yakalayıcı import data_message
 from kelime_bot import OWNER_ID
 
-@Client.on_message(filters.command(["duyuru"], [".", "/"]) & filters.user(OWNER_ID))
-async def duyuru(c:Client, m:Message):
+@Client.on_message(filters.command(["elan"], [".", "/"]) & filters.user(OWNER_ID))
+async def elan(c:Client, m:Message):
     chats = await c.get_messages(OWNER_ID, data_message.message.message_id)
     chats_list = chats.text.split()
         
@@ -17,27 +17,27 @@ async def duyuru(c:Client, m:Message):
         mesaj = m.reply_to_message.text
     else:
         mesaj = m.text[8:]
-    await c.send_message(m.chat.id,f"**Gönerilecek Grup Sayısı:  {len(chats_list)}\nMesajınız:**\n\n__{mesaj}__")
+    await c.send_message(m.chat.id,f"**Göndəriləcək Qrup Sayı:  {len(chats_list)}\nMesajınız:**\n\n__{mesaj}__")
 
 
 
     #----> Gönderme İşlemi <----
-    bas =await c.send_message(m.chat.id, "**Duyuru Yapılmaya Başladı.**")
+    bas =await c.send_message(m.chat.id, "**Elan Göndərilməyə Başladı.**")
     for chat in chats_list:
         try:
             await c.send_message(chat, mesaj, disable_web_page_preview=True)
-            await bas.edit(f"**Duyuru Yapılmaya Başladı.**\n\nGönderildi: {chat}")
+            await bas.edit(f"**Elan Göndərilməyə Başladı.**\n\nGöndərildi: {chat}")
         except:
             pass
         sleep(2)
-    await c.send_message(m.chat.id, "**Duyuru İşlemi Bitti Tüm Gruplara Duyurunuz Yollandı.**")
+    await c.send_message(m.chat.id, "**Elan Göndərmə Prosesi Bitti Bütün Qruplara Elanınəz Göndərildi.**")
     
     
     
     
 
-@Client.on_message(filters.command(["fduyuru"], [".", "/"]) & filters.user(OWNER_ID))
-async def fduyuru(c:Client, m:Message):
+@Client.on_message(filters.command(["felan"], [".", "/"]) & filters.user(OWNER_ID))
+async def felan(c:Client, m:Message):
     chats = await c.get_messages(OWNER_ID, data_message.message_id)
     chats_list = chats.text.split()
         
@@ -47,17 +47,17 @@ async def fduyuru(c:Client, m:Message):
         message_id = m.reply_to_message.message_id
         mesaj = "t.me/" + str(m.chat.username) + "/" + str(message_id)
     else:
-        return m.reply("**Mesajı yönlendirme şeklinde duyuru yapmak için yanıtlayın !!**")
-    await c.send_message(m.chat.id,f"**Gönerilecek Grup Sayısı:  {len(chats_list)}\nMesajınız: [Tıkla]({mesaj})**", disable_web_page_preview=True)
+        return m.reply("**Mesajı yönləndirmə şəklində Elan etmək üçün  yanıtlayın !!**")
+    await c.send_message(m.chat.id,f"**Göndəriləcək Qrup Sayı:  {len(chats_list)}\nMesajınız: [Tıkla]({mesaj})**", disable_web_page_preview=True)
 
 
     #----> Gönderme İşlemi <----
-    bas = await c.send_message(m.chat.id, "**Duyuru Yapılmaya Başladı.**")
+    bas = await c.send_message(m.chat.id, "**Elan Göndərilməyə Başlandı.**")
     for chat in chats_list:
         try:
             await c.forward_messages(chat,m.chat.id, message_id)
-            await bas.edit(f"**Duyuru Yapılmaya Başladı.**\n\nGönderildi: {chat}")
+            await bas.edit(f"**Elan Göndərilməyə Başlandı.**\n\nGöndərildi: {chat}")
         except:
             pass
         sleep(2)
-    await c.send_message(m.chat.id, "**Duyuru İşlemi Bitti Tüm Gruplara Duyurunuz Yollandı.**")
+    await c.send_message(m.chat.id, "**Elan Prosesi Bitti Bütün Qruplara Elanınız Göndərildi.**")
