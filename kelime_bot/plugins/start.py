1c18d8ce119838fc21b29fb6bea482449af3bc98
RaviBey <112951109+RaviBey@users.noreply.github.com>
2023-07-16 04:05:05 +0400

    Update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -40,8 +40,7 @@ HELP = """
  #  Komutlar
 @Client.on_message(filters.command("start"))
 async def start(bot, message):
-  await message.reply_photo("https://telegra.ph/file/9facf7f4d3990d3c93637.jpg",caption=START,reply_markup=keyboard)
-
+  
 @Client.on_message(filters.command("help"))
 async def help(bot, message):
   await message.reply_photo("https://telegra.ph/file/9facf7f4d3990d3c93637.jpg",caption=HELP)
